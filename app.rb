5a8c89fb51f1e05b0fbf9d9ef088b883b00398db
andy <andyvesel@ya.ru>
2016-04-02 19:24:42 +0300

    Split string via regex

--- app.rb
+++ app.rb
@@ -2,10 +2,12 @@ require 'rubygems'
 require 'open-uri'
 
 # requests
-url = 'http://api.seobrain.ru/v1/projects/amvzjvb1/positions?apiKey=d602c8a1-922e-4663-adad-c8cd6e891cde
-'
+url = 'http://api.seobrain.ru/v1/projects?apiKey=d602c8a1-922e-4663-adad-c8cd6e891cde'
 doc = open(url)
 
-doc.each {|k| puts k}
+doc.each do |k|
+  k = k.split(/\W+/)
+  puts k
+end
 # positiions
 # http://api.seobrain.ru/v1/projects/amvzjvb1/positions?apiKey=d602c8a1-922e-4663-adad-c8cd6e891cde
