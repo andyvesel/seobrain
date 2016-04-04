3fa5e378ed9985ed0a1623441a49b4eb270b551e
andyvesel <andyvesel@ya.ru>
2016-04-04 09:53:54 +0300

    Fix regex to select correct numbers

--- app.rb
+++ app.rb
@@ -1,13 +1,10 @@
 require 'rubygems'
 require 'open-uri'
 
-# requests
-url = 'http://api.seobrain.ru/v1/projects?apiKey=d602c8a1-922e-4663-adad-c8cd6e891cde'
+url = 'http://api.seobrain.ru/v1/projects/amvzjvb1/positions?apiKey=d602c8a1-922e-4663-adad-c8cd6e891cde'
 doc = open(url)
 
 doc.each do |k|
-  k = k.split(/\W+/)
-  puts k
+  k = k.scan(/\b\d{1,2}\b/)
+  p k
 end
-# positiions
-# http://api.seobrain.ru/v1/projects/amvzjvb1/positions?apiKey=d602c8a1-922e-4663-adad-c8cd6e891cde
