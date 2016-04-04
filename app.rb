cb4b96dbff4ab40862658c8299c37a3a32e49e4f
andyvesel <andyvesel@ya.ru>
2016-04-04 10:17:42 +0300

    Add open file method

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require 'rubygems'
 require 'open-uri'
 
-url = 'http://api.seobrain.ru/v1/projects/amvzjvb1/positions?apiKey=d602c8a1-922e-4663-adad-c8cd6e891cde'
-doc = open(url)
+file = File.open('config.txt', 'r')
+content = file.read
+url = open(content)
 
-doc.each do |k|
+url.each do |k|
   k = k.scan(/\b\d{1,2}\b/)
   p k
 end
