b1feaeae055eca7f4de57d446a78709a1abfd316
andyvesel <andyvesel@ya.ru>
2016-04-11 11:13:32 +0300

    Add ruby XL gem

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'open-uri'
+require 'rubyXL'
 
 file = File.open('config.txt', 'r')
 content = file.read
@@ -9,3 +10,7 @@ url.each do |k|
   k = k.scan(/\b\d{1,2}\b/)
   p k
 end
+
+workbook = RubyXL::Parser.parse('1.xlsx')
+worksheet = workbook.worksheets[0]
+puts worksheet[1][1]
