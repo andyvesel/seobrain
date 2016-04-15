24a51beb0f7321ac8bbd4a6f499a4b6c7a9eec7a
andyvesel <andyvesel@ya.ru>
2016-04-15 09:16:26 +0300

    Add class Parser

---
+++ parser.rb
@@ -0,0 +1,16 @@
+class Parser
+  
+  def read_file
+    file = File.open('config.txt', 'r')
+    url = file.read
+    @page_data = open(url)
+  end
+
+  def parse_positions
+    @page_data.each do |content|
+      @positions = content.scan(/\b\d{1,2}\b/)
+      p @positions
+    end
+  end
+
+end
