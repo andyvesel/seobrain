f26341cbd8d0d0ab3ebfc91b7ce80f9413dbd42d
andyvesel <andyvesel@ya.ru>
2016-04-27 10:50:16 +0300

    Modify mailer.rb

--- mailer.rb
+++ mailer.rb
@@ -1,11 +1,12 @@
 class Mailer
   
   def mail
+    t = Time.now
     mail = Mail.new do
       from     'andyvesel@ya.ru'
       to       'andyvesel@yandex.ru'
-      subject  'Here is the image you wanted'
-      body     'Hello'
+      subject  'Позиции 9216001.spb.ru'
+      body     "Добрый день. Высылаю отчет по позициям 9216001.spb.ru за 0#{t.month}.#{t.year}"
       add_file :filename => '1.xlsx', :content => File.read('1.xlsx')
     end
 
